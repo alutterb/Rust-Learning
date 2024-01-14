b2c95b4ee7bdfea15e0bb28c4ebce08ad49caed6
alutterb <rmhslutterbach@gmail.com>
2024-01-14 14:18:01 -0600

    tokenization of text data

--- sentiment_analysis/src/main.rs
+++ sentiment_analysis/src/main.rs
@@ -1,5 +1,8 @@
 /// handles command line arguments, initializes main components, and ties together modules
+mod tokenizer;
 
 fn main() {
-    println!("Hello, world!");
+    let file_path = "/home/akagi/home/Rust-Learning/Rust-Learning/sentiment_analysis/data/sample.txt";
+    let tokens = tokenizer::tokenize(file_path);
+    println!("{:?}", tokens);
 }
--- sentiment_analysis/src/tokenizer.rs
+++ sentiment_analysis/src/tokenizer.rs
@@ -2,8 +2,19 @@
 Input - .txt file
 Output - Vector of tokens
 */
+use std::fs;
 
-pub fn tokenize(text: &str) -> Vec<String> {
+fn read_txt_file(file_path: &str) -> String {
+    let file_contents = match fs::read_to_string(file_path) {
+        Ok(contents) => contents,
+        Err(e) => panic!("Error reading file {}: {}", file_path, e)
+    };
+
+    return file_contents
+}
+
+pub fn tokenize(file_path: &str) -> Vec<String> {
+    let text = read_txt_file(file_path);
     // We will use the simple case of splitting on whitespace
     // Does not take into account punctuation, stop words, etc...
     text.split_whitespace().map(|s| s.to_lowercase()).collect()
